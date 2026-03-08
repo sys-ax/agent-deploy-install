cd0cdd4d0007b89341ca34dbd4d597335093dfe4
alejandroyu2 <127617616+alejandroyu2@users.noreply.github.com>
2026-03-08 16:17:16 -0300

    update setup.sh checksum after bugfix

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ readonly REPO_RAW="https://raw.githubusercontent.com/${INSTALLER_REPO}/main"
 # Expected SHA256 of setup.sh inside the private repo.
 # Update this value every time setup.sh changes.
 # Generate with: shasum -a 256 setup.sh
-readonly SETUP_SH_CHECKSUM="b1d5b4e5641deaa21c4d93ef3bbf78a107fc9349744f53d69be73b77218a637d"
+readonly SETUP_SH_CHECKSUM="c1bd159e541f9049de9fd2f7360d8ca31fddc9466e88baab65bb01c3c25871ab"
 
 # Minimum required files from the public installer repo
 readonly REQUIRED_FILES="install.sh install.sh.sig signing-key.pub CHECKSUMS.sha256"
